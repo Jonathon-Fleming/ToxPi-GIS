89a96ed3513b93d75e0f31da899366b910ca43b9
Jonathon-Fleming <65171154+Jonathon-Fleming@users.noreply.github.com>
2021-08-19 17:09:03 -0400

    Update split_coordinates.py

--- Utilities/split_coordinates.py
+++ Utilities/split_coordinates.py
@@ -6,5 +6,6 @@ def split_coordinates(path):
     df[['Latitude','Longitude']] = df.Source.str.split(",",expand = True,)
     del df['Source']
     df.to_csv(path, index = False)
+    
 if __name__ == '__main__':
     split_coordinates(sys.argv[1])
